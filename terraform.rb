88e3e21c034454174bbf8084b231e0d69814cbbc
Jon Barber <jon.barber@acm.org>
2019-03-14 16:33:21 +0000

    Add terraform 0.11.12

---
+++ terraform.rb
@@ -0,0 +1,74 @@
+class Terraform < Formula
+    desc "Tool to build, change, and version infrastructure"
+    homepage "https://www.terraform.io/"
+    url "https://github.com/hashicorp/terraform/archive/v0.11.12.tar.gz"
+    sha256 "ca4a8c816cea5e83362f2fc9e2c0976be0155d0c13f638813aaaa2034786da9a"
+    head "https://github.com/hashicorp/terraform.git"
+  
+    bottle do
+      cellar :any_skip_relocation
+      sha256 "44b4f8b20e24385d31b2ff6e762a76ff80bd669cc767f876afe39c99260912d1" => :mojave
+      sha256 "dcf67d39e25ead434deec4bf3b4104f3895320dd06689ca942299882c127f232" => :high_sierra
+      sha256 "eaa3b0c7c6cccccc6ebafbec5e87b793599d366c884757758bee5bc9a5f431c2" => :sierra
+    end
+  
+    depends_on "go" => :build
+    depends_on "gox" => :build
+  
+    conflicts_with "tfenv", :because => "tfenv symlinks terraform binaries"
+  
+    def install
+      ENV["GOPATH"] = buildpath
+      ENV.prepend_create_path "PATH", buildpath/"bin"
+  
+      dir = buildpath/"src/github.com/hashicorp/terraform"
+      dir.install buildpath.children - [buildpath/".brew_home"]
+  
+      cd dir do
+        # v0.6.12 - source contains tests which fail if these environment variables are set locally.
+        ENV.delete "AWS_ACCESS_KEY"
+        ENV.delete "AWS_SECRET_KEY"
+  
+        ENV["XC_OS"] = "darwin"
+        ENV["XC_ARCH"] = "amd64"
+        system "make", "tools", "test", "bin"
+  
+        bin.install "pkg/darwin_amd64/terraform"
+        prefix.install_metafiles
+      end
+    end
+  
+    test do
+      minimal = testpath/"minimal.tf"
+      minimal.write <<~EOS
+        variable "aws_region" {
+            default = "us-west-2"
+        }
+  
+        variable "aws_amis" {
+            default = {
+                eu-west-1 = "ami-b1cf19c6"
+                us-east-1 = "ami-de7ab6b6"
+                us-west-1 = "ami-3f75767a"
+                us-west-2 = "ami-21f78e11"
+            }
+        }
+  
+        # Specify the provider and access details
+        provider "aws" {
+            access_key = "this_is_a_fake_access"
+            secret_key = "this_is_a_fake_secret"
+            region = "${var.aws_region}"
+        }
+  
+        resource "aws_instance" "web" {
+          instance_type = "m1.small"
+          ami = "${lookup(var.aws_amis, var.aws_region)}"
+          count = 4
+        }
+      EOS
+      system "#{bin}/terraform", "init"
+      system "#{bin}/terraform", "graph"
+    end
+  end
+  

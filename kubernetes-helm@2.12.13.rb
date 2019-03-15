d73ee6f1f7f27a484759631284aaf89400b06069
Jon Barber <jon.barber@acm.org>
2019-03-15 08:37:28 +0000

    helm 2.12.13 formula

---
+++ kubernetes-helm@2.12.13.rb
@@ -0,0 +1,22 @@
+class KubernetesHelmAT21213 < Formula
+  desc "The Kubernetes package manager"
+  homepage "https://helm.sh/"
+  url "https://storage.googleapis.com/kubernetes-helm/helm-v2.12.3-darwin-amd64.tar.gz"
+  sha256 "564a087915e07333d2335face555eaac63accd669fb63672ae1b66a73144ac50"
+
+  bottle :unneeded
+
+  def install
+    bin.install "darwin-amd64/helm"
+    bin.install "darwin-amd64/tiller"
+  end
+
+  test do
+    system "#{bin}/helm", "create", "foo"
+    assert File.directory? "#{testpath}/foo/charts"
+
+    version_output = shell_output("#{bin}/helm version --client 2>&1")
+    assert_match "GitTreeState:\"clean\"", version_output
+    assert_match stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision], version_output if build.stable?
+  end
+end
